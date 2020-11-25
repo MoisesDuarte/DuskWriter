206537ec2f5c69583afe800d3a119699b5d9b499
MoisesDuarte <moisesduartejunior@gmail.com>
2020-11-24 21:02:41 -0300

    Adding capacitor to project

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -30,8 +30,8 @@ main {
   height: 100%;
   display: flex;
   flex-direction: column;
-  justify-content: space-between;
   align-items: center;
+  justify-content: center;
   text-align: center;
   font-size: 10px;
 }
